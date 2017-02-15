0ef810c217f803001de42818a84e1ea64d55cb88
Dimitris Stoikidis <jstoikidis@gmail.com>
2017-02-15 20:56:41 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+my_project
+==========
+
+A Symfony project created on February 11, 2017, 10:39 am.
---
+++ src/BlogBundle/Controller/PageController.php
@@ -0,0 +1,52 @@
+<?php
+// src/Blogger/BlogBundle/Controller/PageController.php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BlogBundle\Entity\Enquiry;
+use BlogBundle\Form\EnquiryType;
+use Symfony\Component\HttpFoundation\Request;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BlogBundle:Page:index.html.twig');
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('BlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo('jstoikidis@gmail.com')
+                    ->setBody($this->renderView('BlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add(
+                    'blogger-notice',
+                    'Your contact enquiry was successfully sent. Thank you!'
+                );
+                return $this->redirect($this->generateUrl('BlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
